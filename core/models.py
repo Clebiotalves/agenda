2e24abb39c7fd4af907f0540b9d64f8621c21fbc
Clebio Teodoro Alves <106354414+Clebiotalves@users.noreply.github.com>
2024-03-08 21:12:31 -0300

    CriCriado tela de eventos

--- core/models.py
+++ core/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
@@ -13,5 +14,9 @@ class Evento(models.Model):
     class Meta:
         db_table = 'evento'
 
+
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%y %H:%M Hrs')
